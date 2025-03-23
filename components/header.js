09ad9663923698fac7697996db4323fe761ff9aa
jasmine lo <jasminelo@Jasmines-MBP-3.attlocal.net>
2025-03-22 18:56:20 -0700

    fix indent

--- components/header.js
+++ components/header.js
@@ -6,124 +6,124 @@ class Header extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
         <style>
-          header {
-            position: sticky;
-            top: 0;
-            background: #fff;
-            color: #333;
-            padding: 1rem 0;
-            text-align: center;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding-left: 20px;
-            padding-right: 20px;
-        }
-
-        header nav {
-            display: flex;
-            position: relative;
-            /* white-space: nowrap; */
-        }
-
-        nav ul {
-            list-style: none;
-            padding: 0;
-            margin: 0;
-            display: flex;
-            justify-content: center;
-        }
-
-        nav ul li {
-            margin: 0 15px;
-        }
-
-        nav ul li a {
-            color: #333;
-            text-decoration: none;
-        }
-
-        header nav ul li a:hover{
-            color:#bcbaba;
-        }
-
-        .nav-left {
-            flex: 1;
-            justify-content: flex-start;
-        }
-
-        #capsule-items{
-            position: relative;
-            list-style: none;
-        }
-
-        #capsule-items a:hover{
-            color:#bcbaba;
-        }
-
-        #dropdown-items {
-            display: none;
-            position: absolute;
-            font-size: 11px;
-            white-space: nowrap;
-            background-color: #fff;
-        }
-
-        #dropdown-items li {
-            margin: 5px 0px;
-        }
-
-        .nav-left #dropdown-items li {
-            margin-right: 5px;
-        }
-
-        .nav-right #dropdown-items li {
-            margin-left: 5px;
-        }
-        
-        #capsule-items:hover #dropdown-items {
-            display: block;
-            text-align: left;
-            left: 0px;
-            padding:0;
-        }
-
-        #dropdown-items {
-            animation: animateToBottom .3s;
-        }
-
-        @keyframes animateFromBottom {
-            from {
-            transform: translate3d(0, 10px, 0);
-            opacity: 0
+            header {
+                position: sticky;
+                top: 0;
+                background: #fff;
+                color: #333;
+                padding: 1rem 0;
+                text-align: center;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding-left: 20px;
+                padding-right: 20px;
             }
-            to {
-            transform: translate3d(0, 0, 0);
-            opacity: 1
-            }
-        }
 
-        @keyframes animateToBottom {
-            from {
-            transform: translate3d(0, -5px, 0);
-            opacity: 1
+            header nav {
+                display: flex;
+                position: relative;
+                /* white-space: nowrap; */
             }
-            to {
+
+            nav ul {
+                list-style: none;
+                padding: 0;
+                margin: 0;
+                display: flex;
+                justify-content: center;
+            }
+
+            nav ul li {
+                margin: 0 15px;
+            }
+
+            nav ul li a {
+                color: #333;
+                text-decoration: none;
+            }
+
+            header nav ul li a:hover{
+                color:#bcbaba;
+            }
+
+            .nav-left {
+                flex: 1;
+                justify-content: flex-start;
+            }
+
+            #capsule-items{
+                position: relative;
+                list-style: none;
+            }
+
+            #capsule-items a:hover{
+                color:#bcbaba;
+            }
+
+            #dropdown-items {
+                display: none;
+                position: absolute;
+                font-size: 11px;
+                white-space: nowrap;
+                background-color: #fff;
+            }
+
+            #dropdown-items li {
+                margin: 5px 0px;
+            }
+
+            .nav-left #dropdown-items li {
+                margin-right: 5px;
+            }
+
+            .nav-right #dropdown-items li {
+                margin-left: 5px;
+            }
+            
+            #capsule-items:hover #dropdown-items {
+                display: block;
+                text-align: left;
+                left: 0px;
+                padding:0;
+            }
+
+            #dropdown-items {
+                animation: animateToBottom .3s;
+            }
+
+            @keyframes animateFromBottom {
+                from {
+                transform: translate3d(0, 10px, 0);
+                opacity: 0
+                }
+                to {
                 transform: translate3d(0, 0, 0);
                 opacity: 1
+                }
             }
-        }
 
-        #store-items{
-            position: relative;
-        }
-        
-        #store-items:hover #dropdown-items {
-            display: block;
-            text-align: right;
-            right: 0px;
-            padding: 0;
-        }
+            @keyframes animateToBottom {
+                from {
+                transform: translate3d(0, -5px, 0);
+                opacity: 1
+                }
+                to {
+                    transform: translate3d(0, 0, 0);
+                    opacity: 1
+                }
+            }
+
+            #store-items{
+                position: relative;
+            }
+            
+            #store-items:hover #dropdown-items {
+                display: block;
+                text-align: right;
+                right: 0px;
+                padding: 0;
+            }
         </style>
         <header>
             <nav class="nav-left">
